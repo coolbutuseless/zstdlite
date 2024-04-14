7d941f877443feb5080627032d48961f3dfba7ad
mikefc <coolbutuseless@users.noreply.github.com>
2024-04-14 22:01:51 +0000

    add simple zstdfile test

---
+++ tests/testthat/test-zstdfile.R
@@ -0,0 +1,13 @@
+
+
+test_that("zstdfile works", {
+  tmp <- tempfile()
+  dat <- as.raw(1:255)
+  writeBin(dat, zstdfile(tmp, level = 20))
+  tst <- readBin(zstdfile(tmp),  raw(), 1000)
+  expect_identical(tst, dat)
+  
+  #txt <- as.character(mtcars)
+  #writeLines(txt, zstdfile(tmp))
+  #readLines(zstdfile(tmp))
+})
